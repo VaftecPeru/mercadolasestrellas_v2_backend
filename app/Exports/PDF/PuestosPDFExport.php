30a3a53e456a94a4188b532654ce63d467df801c
Danny Christian Rojas Garcia <danny.christian.rojas@gmail.com>
2026-08-11 09:18:53 -0500

    Update PuestosPDFExport.php

--- app/Exports/PDF/PuestosPDFExport.php
+++ app/Exports/PDF/PuestosPDFExport.php
@@ -21,7 +21,8 @@ class PuestosPDFExport {
         'puesto' => $puesto->numero_puesto ?? '------', 
         'area' => $puesto->area ?? '------', 
         'giro' => data_get($puesto, 'gironegocio.nombre', '------'), 
-        'socio' => trim(data_get($puesto, 'socio.nombres', '').' '.data_get($puesto, 'socio.apellido_paterno', '').' '.data_get($puesto, 'socio.apellido_materno', '')) ?: '------', 
+        // el nombre del socio desde la tabla personas
+        'socio' => data_get($puesto, 'socio.persona.nombre_completo', '------') ?: data_get($puesto, 'socio.usuario.nombre_usuario', '------'),
         'inquilino' => trim(data_get($puesto, 'inquilino.nombre', '').' '.data_get($puesto, 'inquilino.apellido_paterno', '').' '.data_get($puesto, 'inquilino.apellido_materno', '')) ?: '------', 
         'estado' => $puesto->estado === '1' ?  'Libre' : 'Ocupado',
         'fecha_registro' => $puesto->fecha_registro ?? '------', 
